924441b7ba997dccdb5c3a9a6aa9b94cb5b85ff3
Almale02 <gaspardaniel080716@gmail.com>
2022-03-24 21:35:21 +0100

    second

--- scripts/main/user_input/mouse_input.py
+++ scripts/main/user_input/mouse_input.py
@@ -4,12 +4,12 @@ import scripts.main.datas.map_datas as map_datas
 def get_tile_pos(mouse_pos,type):
     if type == "row":
         return mouse_pos[1] // (750 // 9)
-    if type == "column":
+    if type  == "column":
         return mouse_pos[0] // (750 // 9)
     if type == "block":
         return  (mouse_pos[0] // (750 // 3), mouse_pos[1] // (750 // 3))
     if type == "tile":
-        return (mouse_pos[0] // 9, mouse_pos[1] // 9)
+        return (mouse_pos[0] // (750 // 9), mouse_pos[1] // (750 // 9))
 
 def tile_input():
     pos = tuple()
@@ -17,7 +17,8 @@ def tile_input():
         pos = pygame.mouse.get_pos()
         print(pos)
         map_datas.blocks[get_tile_pos(pos,"block")[0]][get_tile_pos(pos,"block")[1]][get_tile_pos(pos,"tile")[0] % 3][get_tile_pos(pos,"tile")[1] % 3] = map_datas.current_num
-        print(map_datas.blocks[get_tile_pos(pos,"block")[0]][get_tile_pos(pos,"block")[1]][get_tile_pos(pos,"tile")[0] % 3][get_tile_pos(pos,"tile")[1] % 3])
+        map_datas.all_tiles[get_tile_pos(pos,"tile")[0]][get_tile_pos(pos,"tile")[1]] = map_datas.current_num
+        print(map_datas.all_tiles[get_tile_pos(pos,"tile")[0]][get_tile_pos(pos,"tile")[1]])
 
 
 
